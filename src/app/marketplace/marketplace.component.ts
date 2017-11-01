d35c932d95270d83c52d9a3620ccd9ae3ee93821
Guest User <Guest@epicodus-27.local>
2017-11-01 09:47:04 -0700

    added services and providers for album details

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
+
 export class MarketplaceComponent implements OnInit {
-  constructor(private router: Router) { }
+  albums: Album[];
+
+  constructor(
+    private router: Router,
+    private albumService: AlbumService
+  ) {}
 
   ngOnInit() {
+    this.albums = this.albumService.getAlbums();
   }
-
-  albums: Album[] = [
-    new Album("Speaking In Tongues", "Talking Heads", "The fifth studio album by Talking Heads, it is also their highest charting album on the American Billboard 200", 1),
-    new Album("Make It Big", "Wham!", "Certified 6x Platinum, the album has sold over 10 million copies worldwide.", 2),
-    new Album("Skid Row", "Skid Row", "Debut studio album from Skid Row produced 3 singles and brought the group nationwide popularity.", 3),
-    new Album("Van Halen", "Van Halen", "Debut studio album that contains many of the bands signature songs.", 4),
-    new Album("Let's Dance", "David Bowie", "Bowie's fifteenth studio album, it was produced by Nile Rodgers and contains three of Bowie's most successful singles.", 5)
-  ];
 
   goToDetailPage(clickedAlbum: Album) {
     this.router.navigate(['albums', clickedAlbum.id]);
---
+++ src/app/mock-albums.ts
@@ -0,0 +1,9 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album [] = [
+  new Album("Speaking In Tongues", "Talking Heads", "The fifth studio album by Talking Heads, it is also their highest charting album on the American Billboard 200", 1),
+  new Album("Make It Big", "Wham!", "Certified 6x Platinum, the album has sold over 10 million copies worldwide.", 2),
+  new Album("Skid Row", "Skid Row", "Debut studio album from Skid Row produced 3 singles and brought the group nationwide popularity.", 3),
+  new Album("Van Halen", "Van Halen", "Debut studio album that contains many of the bands signature songs.", 4),
+  new Album("Let's Dance", "David Bowie", "Bowie's fifteenth studio album, it was produced by Nile Rodgers and contains three of Bowie's most successful singles.", 5)
+];
